a69a39ee1d6a79ebc2b96d4d797cb2fd8b9be6de
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-09 21:26:29 +0800

    use useState

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,54 @@
-import { User } from 'lucide-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { User } from 'lucide-react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleUser = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <header className='bg-white shadow-md'>
-        <>
-        <div className='py-4 shadow-md '>
-            <ul className='container mx-auto flex flex-wrap justify-between md:flex-row px-4 md:px-2 items-center'>
-                <div className='flex gap-4'>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                      <li>
-                        <Link to='/'>About</Link>
-                    </li>
-                      <li>
-                        <Link to='/'>FAQ</Link>
-                    </li>
-                     <li>
-                        <Link to='/'>Contact</Link>
-                    </li>
-                </div>
-                <div>
-                    <li>
-                        <Link to='/'>Sign</Link>
-                    </li>
-                      <li>
-                        <Link to='/'>My Account</Link>
-                    </li>
-                </div>
-                <User size={40} className='bg-gray-200 p-2 text-black rounded cursor-pointer'/>
-            </ul>
+    <header className="bg-white shadow-md">
+      <>
+        <div className="py-4 shadow-md ">
+          <ul className="container mx-auto flex flex-wrap justify-between md:flex-row px-4 md:px-2 items-center relative">
+            <div className="flex gap-4">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/">FAQ</Link>
+              </li>
+              <li>
+                <Link to="/">Contact</Link>
+              </li>
+            </div>
+            <div
+              className={
+              `${isOpen ? "flex flex-col absolute right-0 md:right-0 top-12 z-10 bg-zinc-50 p-4 gap-4" : "hidden" }`
+              }
+            >
+              <li>
+                <Link to="/">Sign</Link>
+              </li>
+              <li>
+                <Link to="/">My Account</Link>
+              </li>
+            </div>
+            <User
+              size={40}
+              className="bg-gray-200 p-2 text-black rounded cursor-pointer"
+              onClick={handleUser}
+            />
+          </ul>
         </div>
-        </>
+      </>
     </header>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
