cca54e27624b6d66ec65e6fdf34daa58c4068e2c
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-10 21:10:24 +0800

    organized things

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { ShoppingCart, User } from 'lucide-react';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/img/logo.png';
+import logo from '../assets/img/Logo/logo.png';
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
