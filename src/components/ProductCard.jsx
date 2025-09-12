1e0a82ade41d0d8c4e596336ea635a6372c54d1e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-12 23:46:48 +0800

    details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,29 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { formatPrice } from '../utils/format';
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
   return (
-  <Link>
-  <div className=' w-56 h-72 shadow-lg rounded-md cursor-pointer'>
-    <div className='w-full h-40 flex items-center justify-center  rounded-md'>
-    <img src={product.image} alt={product.name} className='max-h-full object-contain' />
-  </div>
-  <h3 className='mt-3  font-semibold text-base items-center text-center'>{product.name}</h3>
-    <p className='text-sm text-gray-600 text-center mt-1'>₱{formatPrice(product.price)}</p>
-    <p className='items-center text-center mt-1'>{product.category}</p>
-    <p className='items-center text-center font-semibold'>View Details</p>
-  </div>
-  </Link>
-  )
+    <Link>
+      <div className=" w-56 h-72 shadow-lg rounded-md cursor-pointer">
+        <div className="w-full h-40 flex items-center justify-center  rounded-md">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="max-h-full object-contain"
+          />
+        </div>
+        <div className='p-4'>
+          <h3 className="mt-3  font-semibold text-base items-center text-center">
+            {product.name}
+          </h3>
+          <div className="flex justify-between  mt-4">
+            <p>₱{formatPrice(product.price)}</p>
+            <p>View Details</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
