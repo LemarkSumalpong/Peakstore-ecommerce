c3a47407d7f2381ef5186fbaf7f1d47bb3342a8a
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-12 23:42:02 +0800

    details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,9 +8,10 @@ function ProductCard({product}) {
     <div className='w-full h-40 flex items-center justify-center  rounded-md'>
     <img src={product.image} alt={product.name} className='max-h-full object-contain' />
   </div>
-  <h3 className='mt-3 font-semibold text-base items-center text-center'>{product.name}</h3>
+  <h3 className='mt-3  font-semibold text-base items-center text-center'>{product.name}</h3>
     <p className='text-sm text-gray-600 text-center mt-1'>₱{formatPrice(product.price)}</p>
     <p className='items-center text-center mt-1'>{product.category}</p>
+    <p className='items-center text-center font-semibold'>View Details</p>
   </div>
   </Link>
   )
