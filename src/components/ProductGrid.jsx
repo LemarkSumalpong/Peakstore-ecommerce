c79ae16fdcb560f6141c2c084413e81950293b25
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-12 15:50:01 +0800

    product time

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import Product from '../../Product';
+import ProductCard from './ProductCard';
 
 function ProductGrid() {
+  console.log(Product);
   return (
-    <div>
-      ...
-    </div>
+   <div className='grid grid-col-1 md:grid-cols2 lg:grid-cols-3 xl:grid-cols-4 '>
+    {Product.map ((product) => {
+      return <ProductCard/>
+    })}
+   </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
     <div>
       <div className="bg"></div>
       <div className="container mx-auto my-10 px-4">
-        <div className="flex gap-4">
+        <div className="flex gap-4 overflow-x-auto srollbar-hide">
           {Categories.map((category) =>{
             return (
               <button className="bg-gray-300 py-2 px-4 rounded-md text-black  active:scale-105 hover:bg-zinc-400 transition-all ease-in" key={category}>
