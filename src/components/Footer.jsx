bcf946ab3b5233c1d633958e80337562bf8e17ca
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-13 16:10:32 +0800

    footer more footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
-import { ArrowRight } from 'lucide-react';
+import {
+  ArrowRight,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Mail,
+  Twitter,
+} from 'lucide-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -20,15 +28,26 @@ function Footer() {
               </div>
               <div className="py-4">
                 <h3 className="text-blue-400 uppercase">Or Email Us At</h3>
-                <button className="flex items-center gap-2  cursor-pointer text-sm font-medium bg-slate-400 text-black py-3 px-6  border-gray-300 rounded-full hover:bg-gray-100 hover:border-gray-400 transition">
-                  <span>Get Started</span>
-                  <ArrowRight size={20} className="shrink-0" />
+                <button className="bg-[#18192e] text-sm  border-0 text-white px-6 py-3 rounded-full flex items-center gap-3 cursor-pointer hover:bg-gray-800 transition">
+                  <span>sumalponglemark2@gmail.com</span>
+                  <Mail size={16} className="shrink-0" />
                 </button>
               </div>
             </div>
-            <p className="text-white text-center py-6">
-              &copy; 2024 PeakStore. All rights reserved.
-            </p>
+            <div className="border-t border-gray-700">
+              <div className="flex items-center justify-between py-6">
+                <p className="text-blue-400 text-sm">
+                  &copy; 2024 PeakStore. All rights reserved.
+                </p>
+
+                <div className="flex gap-4 text-white">
+                  <Facebook className="cursor-pointer hover:text-blue-400 transition" />
+                  <Linkedin className="cursor-pointer hover:text-blue-500 transition" />
+                  <Twitter className="cursor-pointer hover:text-sky-400 transition" />
+                  <Instagram className="cursor-pointer hover:text-pink-400 transition" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
