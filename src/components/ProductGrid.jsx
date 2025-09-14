158adb107952eae8c194188d3f4b09cd290c20ec
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-14 16:06:22 +0800

    Search search search is workinggg arfrggg

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,7 +1,11 @@
 import Product from '../../Product';
 import ProductCard from './ProductCard';
+import { useSelector } from 'react-redux';
 
 function ProductGrid() {
+
+  const Product = useSelector((state) => state.product.filteredItems);
+
   console.log(Product);
   return (
    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
