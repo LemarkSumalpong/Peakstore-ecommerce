0efcd7f378dbef4b0e37e9e99313350003c1860e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-17 21:35:38 +0800

    auth account

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -12,9 +12,9 @@ function MyAccount() {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-white shadow-md rounded-lg p-6 w-80">
-        <h2 className="text-xl font-bold mb-4">My Account</h2>
+    <div className="bg flex justify-center items-center h-screen">
+      <div className="bg-white border-gray-800 shadow-lg rounded-lg p-6 w-80">
+        <h2 className="text-xl font-bold mb-4 text-center">My Account</h2>
         <p><span className="font-semibold">Username:</span> {user.username}</p>
         <p><span className="font-semibold">Email:</span> {user.email}</p>
       </div>
